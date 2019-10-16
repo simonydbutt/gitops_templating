9a4025ef3cdff83e93acf64232a557415b748ca7
simonydbutt <simonydbutt@gmail.com>
2019-10-17 00:00:07 +0100

    init commit

---
+++ main/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  type = "string"
+  default = "gitops-prod"
+}
+
+variable "region" {
+  type = "string"
+  default = "us-central1"
+}
+
+variable "enabled_services" {
+  type = "list"
+  default = [
+    "bigquery.googleapis.com"
+  ]
+}
